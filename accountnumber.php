722bf0c05a150ac4a9abe89b3d02303f0ececcd0
Cth1003 <ItsAMeCth@gmail.com>
2016-09-18 16:33:25 +0100

    Update accountnumber.php

--- accountnumber.php
+++ accountnumber.php
@@ -1,6 +1,6 @@
 <?php
 $accountnumber = file_get_contents("http://kesbook.cf/users/". $_GET["username"]. "/an.txt");
-$convertedarray = array('bool',$accountnumber,"\xc3\xa9");
+$convertedarray = array('bool',$accountnumber);
 
 echo json_encode($convertedarray), "\n";
 ?>
