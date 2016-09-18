449e9c453550771237e4f427c1b2c73657504a82
Cth1003 <ItsAMeCth@gmail.com>
2016-09-18 16:32:16 +0100

    Fixed "0" Bug.

--- accountnumber.php
+++ accountnumber.php
@@ -1,4 +1,6 @@
 <?php
 $accountnumber = file_get_contents("http://kesbook.cf/users/". $_GET["username"]. "/an.txt");
-echo '{"result":'$accountnumber'"}';
+$convertedarray = array('bool',$accountnumber,"\xc3\xa9");
+
+echo json_encode($convertedarray), "\n";
 ?>
