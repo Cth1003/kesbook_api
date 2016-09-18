20058731774d56ee59b70d01e69231262fef07f3
Cth1003 <ItsAMeCth@gmail.com>
2016-09-18 16:33:52 +0100

    Update verifyed.php

--- verifyed.php
+++ verifyed.php
@@ -1,6 +1,6 @@
 <?php
 $boolverifyed = file_get_contents("http://kesbook.cf/users/". $_GET["username"]. "/ver.txt");
-$convertedarray = array('bool',$boolverifyed,"\xc3\xa9");
+$convertedarray = array('bool',$boolverifyed);
 
 echo json_encode($convertedarray), "\n";
 ?>
