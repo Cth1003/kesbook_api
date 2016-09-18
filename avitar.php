bc3e51aaa0ed9727c7b07a67ac5c9de40f5ce97e
Cth1003 <ItsAMeCth@gmail.com>
2016-09-18 16:32:18 +0100

    Fixed "0" Bug.

--- avitar.php
+++ avitar.php
@@ -1,4 +1,6 @@
 <?php
 $kesbookavitar = file_get_contents("http://kesbook.cf/users/". $_GET["username"]. "/ai.txt");
-echo '{"avitar":'+$kesbookavitar+'"}';
+$convertedarray = array('avitar',$kesbookavitar,"\xc3\xa9");
+
+echo json_encode($convertedarray), "\n";
 ?>
