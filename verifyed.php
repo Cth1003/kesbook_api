7f206da43871ab758d5cb17e6e1b6fc0d52a5d52
kessanro <kessanro@hotmail.com>
2016-09-18 14:46:06 +0100

    fixed errors

--- verifyed.php
+++ verifyed.php
@@ -1,4 +1,4 @@
 <?php
-$boolverifyed = file_get_contents("http://kesbook.cf/users/" + $_GET["username"] + "/ver.txt");
+$boolverifyed = file_get_contents("http://kesbook.cf/users/". $_GET["username"]. "/ver.txt");
 echo "result: ". $boolverifyed;
 ?>
