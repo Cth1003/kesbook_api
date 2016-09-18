ca6aa577477eebaccd8926eb915227b2d18f9925
Cth1003 <ItsAMeCth@gmail.com>
2016-09-18 16:33:38 +0100

    Update avitar.php

--- avitar.php
+++ avitar.php
@@ -1,6 +1,6 @@
 <?php
 $kesbookavitar = file_get_contents("http://kesbook.cf/users/". $_GET["username"]. "/ai.txt");
-$convertedarray = array('avitar',$kesbookavitar,"\xc3\xa9");
+$convertedarray = array('avitar',$kesbookavitar);
 
 echo json_encode($convertedarray), "\n";
 ?>
